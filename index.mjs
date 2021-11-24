9929291be201aa05ba4f3657419b8358c001971b
laibin5 <laibin5@gdii-yueyun.com>
2021-11-24 12:11:31 +0800

    init project“

---
+++ index.mjs
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import {$,question,chalk,argv} from 'zx'
+
+question('xxx')
+// node index.mjs --images=xx
+// const {images} = argv
+// console.log(images)
+
+
+// await $`date`
+// console.log(chalk.blue('fuck'))
+// await question('fuck u?', {
+//     choices: ['yes', 'no']
+// })
+// console.log(chalk.blue(un))
+// let build;
+try {
+    // console.log(chalk.greenBright(' 🍺 Success!'))
+    // await $`docker build -t xxxx .`
+    // await $`docker tag xxxx ddd`
+} catch(e) {
+    // console.log(build.exitCode)
+}
